5e63e1c9b8967f541e0a30da96a1335af0f7fb25
Lee Katz - Aspen <gzu2@cdc.gov>
2024-05-23 14:35:45 -0400

    inline python documentation

--- lib/hash_sequence.py
+++ lib/hash_sequence.py
@@ -1,7 +1,15 @@
 import hashlib
 
 def hash_sequence(sequence: str) -> str:
+    """
+    Hashes a given sequence using the MD5 algorithm.
 
+    Args:
+        sequence (str): The sequence to be hashed.
+
+    Returns:
+        str: The hashed result as a string.
+    """
     md5 = hashlib.md5(sequence.encode("utf-8"))
     max_bits_in_result = 56
     p = (1 << max_bits_in_result) - 1
@@ -13,6 +21,16 @@ def hash_sequence(sequence: str) -> str:
     return str(result)
 
 def reduceMd5(md5: str, max_bits_in_result: int) -> int:
+    """
+    Reduces a given MD5 hash to a specified number of bits.
+
+    Args:
+        md5 (str): The MD5 hash to be reduced.
+        max_bits_in_result (int): The maximum number of bits in the resulting hash.
+
+    Returns:
+        int: The reduced hash as an integer.
+    """
     p = (1 << max_bits_in_result) - 1
     rest = int(md5, 16)
     result = 0
