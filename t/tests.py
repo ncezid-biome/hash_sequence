b5b31f563b9c8e83bbd5363c49657e117661628e
Arzoo Patel <azoopatel@gmail.com>
2024-05-28 16:53:57 -0400

    added python unit tests

--- t/tests.py
+++ t/tests.py
@@ -1,20 +1,23 @@
+import importlib.util
 import sys
-sys.path.append('/home/runner/work/hash_sequence/hash_sequence/lib/')
+spec = importlib.util.spec_from_file_location("hash_sequence", "../../lib/hash_sequence.py")
+hash_sequence = importlib.util.module_from_spec(spec)
+sys.modules["hash_sequence"] = hash_sequence
+spec.loader.exec_module(hash_sequence)
 
 import hashlib
-import hash_seqeunce
 
 seq = 'ACGT'
 
 def test_answer():
     # hash ACGT
-    assert hash_sequence.hash_seqeunce(seq) == '46054055969511637'
+    assert hash_sequence.hash_sequence(seq) == '46054055969511637'
     # format md5 hash of ACGT
     md5_seq = hashlib.md5(seq.encode("utf-8")).hexdigest()
     assert hash_sequence.reduceMd5(md5_seq, 56) == 46054055969511637
     # hash ACGT*100
     long_seq = 'ACGT' * 100
-    assert hash_sequence.hash_seqeunce(long_seq) == '53658030375034075'
+    assert hash_sequence.hash_sequence(long_seq) == '53658030375034075'
     # format md5 hash of ACGT*100
     md5_long_seq = hashlib.md5(long_seq.encode("utf-8")).hexdigest()
     assert hash_sequence.reduceMd5(md5_long_seq, 56) == 53658030375034075
