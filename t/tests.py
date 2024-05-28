6ae17d7b93bdd3c7514261f497cf57d4b95d9dbe
Arzoo Patel <azoopatel@gmail.com>
2024-05-28 16:32:02 -0400

    added python unit tests

---
+++ t/tests.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.insert(0, '..')
+
+import hashlib
+from lib import hash_seqeunce
+
+seq = 'ACGT'
+
+def test_answer():
+    # hash ACGT
+    assert hash_sequence.hash_seqeunce(seq) == '46054055969511637'
+    # format md5 hash of ACGT
+    md5_seq = hashlib.md5(seq.encode("utf-8")).hexdigest()
+    assert hash_sequence.reduceMd5(md5_seq, 56) == 46054055969511637
+    # hash ACGT*100
+    long_seq = 'ACGT' * 100
+    assert hash_sequence.hash_seqeunce(long_seq) == '53658030375034075'
+    # format md5 hash of ACGT*100
+    md5_long_seq = hashlib.md5(long_seq.encode("utf-8")).hexdigest()
+    assert hash_sequence.reduceMd5(md5_long_seq, 56) == 53658030375034075
